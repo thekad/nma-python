0377ed2f462ba58f5be5c8b4e887705d4837bd47
Jorge Gallegos <kad@eventbrite.com>
2011-09-21 23:18:02 -0700

    Adding simple setup file

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# -*- mode: python; sh-basic-offset: 4; indent-tabs-mode: nil; coding: utf-8 -*-
+# vim: tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8
+
+import setuptools
+from nma import NAME, VERSION
+
+
+setuptools.setup(
+    name = NAME,
+    version = VERSION,
+    author = 'Jorge Gallegos',
+    author_email = 'kad@blegh.net',
+    description = 'A library/cli tool for notifymyandroid (http://nma.usk.bz)',
+    install_requires = [
+        'argparse',
+        'httplib2'
+    ],
+    packages = setuptools.find_packages('.'),
+    zip_safe = False,
+    url = 'https://github.com/thekad/nma-python',
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
